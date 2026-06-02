40a4bd7a02562ead460d264e0a9a65dcf568d367
PavanSuryaChintada <carbonisgod17@gmail.com>
2026-06-02 23:21:34 +0530

    remove Google Ads placeholder sections

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -9,35 +9,22 @@ import { TestimonialsSection } from "../components/TestimonialsSection";
 import { ProcessSection } from "../components/ProcessSection";
 import { ContactSection } from "../components/ContactSection";
 import { CtaSection } from "../components/CtaSection";
-import { AdPlaceholderSection } from "../components/AdPlaceholderSection";
 
 export function Home() {
   return (
     <main>
       <HeroSection />
-      
-      <AdPlaceholderSection size="leaderboard" />
-      
       <BrandStory />
       <CtaSection darkTheme={true} />
-      
       <ProjectsSection />
       <ArchitectureSection />
       <CtaSection darkTheme={false} />
-      
-      <AdPlaceholderSection size="medium-rectangle" />
-      
       <ServicesSection />
       <DesignPhilosophy />
       <CtaSection darkTheme={true} />
-      
       <MaterialsSection />
-      
-      <AdPlaceholderSection size="leaderboard" />
-      
       <TestimonialsSection />
       <CtaSection darkTheme={false} />
-      
       <ProcessSection />
       <ContactSection />
     </main>
